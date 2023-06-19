111d386c409bfd4e5bc9ce0ba852e2492ef6ab44
quentinmathieu <quentin.mth@gmail.com>
2023-06-19 15:00:20 +0200

    Code color added for the response

--- src/Service/OpenAiService.php
+++ src/Service/OpenAiService.php
@@ -22,15 +22,15 @@ class OpenAiService
         
         $complete =  $open_ai->completion([
             'model' => 'text-davinci-003',
-            'prompt' => ('thanks to this message : '.$commitMsg . "\n and to this changes that have been release on this commit :". $commitContent . "\n Explain what happens in the code, and quote de code with '<blockquote></blockquote>' (it's for a tutorial). "),
-            'max_tokens' => 1000,
+            'prompt' => ('thanks to this message : '.$commitMsg . "\n and to this changes that have been release on this commit :". $commitContent . "\n Explain what happens in the code, and quote de code with '<code></code>' (it's for a tutorial). "),
+            'max_tokens' => 100,
             'frequency_penalty' => 0.5,
             'presence_penalty' => 0,
         ]);
         $json = json_decode($complete, true);
         // dd($json);
-
-
+    
+        
         return $json['choices'][0]['text'];
     }
 }
